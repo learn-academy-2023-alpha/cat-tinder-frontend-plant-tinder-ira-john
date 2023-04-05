26dccb20b9daaf6125f0803f64d89f7d9d2bef4c
iraholmes <ira.holmes63@gmail.com>
2023-04-05 11:59:09 -0700

    Frontend Structure almost completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import PlantIndex from "./pages/PlantIndex"
+import PlantNew from "./pages/PlantNew"
+import PlantShow from "./pages/PlantShow"
+import PlantEdit from "./pages/PlantEdit"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [mockPlants, setMockPlants] = useState(plants)
+  console.log(mockPlants)
+  return(
+      <>
+      <Header />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/plantindex" element={<PlantIndex />} /> 
+          <Route path="/plantnew" element={<PlantNew />} />
+          <Route path="/plantshow" element={<PlantShow />} />
+          <Route path="/plantedit" element={<PlantEdit />} />
+          <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+  </>
+  )
 }
 
 export default App;
