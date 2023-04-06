0c3f733f9d980e966a8da4bd044324fd1333dadf
iraholmes <ira.holmes63@gmail.com>
2023-04-06 11:28:26 -0700

    Read-Functionality is completed

--- src/pages/PlantIndex.js
+++ src/pages/PlantIndex.js
@@ -1,12 +1,46 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {Card, CardBody, CardSubtitle, CardTitle, Button} from 'reactstrap'
 
 
-const PlantIndex = () => {
+
+const PlantIndex = ({plants}) => {
     return (
-        <>
-            <div>Plant Tinder!</div>
-        </>
-    )   
-}
-
+      <>
+        <h1>Here are the lovley plants!</h1>
+        <p>Check out their Roots</p>
+        <div className='card-flex'>
+          {plants.map((plant, index) => {
+            return (
+              <Card
+                key={index}
+                style={{
+                  width: '18rem'
+                }}
+              >
+                <img
+                  alt={plant.name}
+                  src={plant.image}
+                />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    {plant.name}
+                  </CardTitle>
+                  <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                  >
+                    {plant.age}
+                  </CardSubtitle>
+                  <Button>
+                    <NavLink to={`/plantshow/${plant.id}`}>More About Me</NavLink>
+                  </Button>
+                </CardBody>
+              </Card>
+            )
+          })}
+        </div>
+      </>
+    )
+  }
 export default PlantIndex
